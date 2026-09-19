11ccfc85a4f0ba3531855d85bb2855a54fe695f1
yunyingk <haoda2195176187@gmail.com>
2026-09-19 10:38:59 +0800

    fix: update correct sha256 checksum for v0.10.30

--- Formula/seal-use.rb
+++ Formula/seal-use.rb
@@ -8,7 +8,7 @@ class SealUse < Formula
   depends_on :macos
 
   url "https://git.ying-qing.cn/gitea/seal-use/releases/download/v#{version}/seal-use-darwin-arm64.tar.gz"
-  sha256 "671b62a99445fedb2fe4503cbca5c71b635c91fcf31215167f8e77a2faea812f"
+  sha256 "671b62a99445fedb2fe4503cbca5cccce2ccc289ed6766def4e29f57d04fe1c2"
 
   def install
     bin.install "seal-use-darwin-arm64" => "seal-use"
