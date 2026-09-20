8167536d71b8c92700b6ecee0d147b26eb47394e
yunyingk <haoda2195176187@gmail.com>
2026-09-20 15:49:15 +0800

    chore(release): bump seal-use to 0.10.32

--- Formula/seal-use.rb
+++ Formula/seal-use.rb
@@ -1,14 +1,14 @@
 class SealUse < Formula
   desc "Seal tenant and Hose approval automation CLI"
   homepage "https://git.ying-qing.cn/gitea/seal-use"
-  version "0.10.31"
+  version "0.10.32"
   license "MIT"
 
   depends_on arch: :arm64
   depends_on :macos
 
   url "https://git.ying-qing.cn/gitea/seal-use/releases/download/v#{version}/seal-use-darwin-arm64.tar.gz"
-  sha256 "936f7e7febd54d4a7d5f1954508aa4cbfc291bfbbb1b29a10047be2ea45af950"
+  sha256 "fda514b2ba6e160b494a8688e64ede0c370a5866957635922957a614580fe5ee"
 
   def install
     bin.install "seal-use-darwin-arm64" => "seal-use"
